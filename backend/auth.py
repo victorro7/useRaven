7ab5f85a32e54a482a90d6f072f92f4a14a05212
victor osunji <victorosunji@gmail.com>
2025-03-10 11:39:02 -0400

    upt cors

--- backend/auth.py
+++ backend/auth.py
@@ -14,6 +14,7 @@ CLERK_SECRET_KEY = os.getenv("CLERK_SECRET_KEY")
 
 # Define your allowed origins (replace with your frontend URL)
 origins = [
+    "https://useraven.app",
     "http://localhost:3000",
 ]
 
